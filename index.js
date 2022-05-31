a0e9d937a1fea9590be02dce8719f6c3b3dd71bb
Arman <villegasarman23@gmail.com>
2022-05-31 07:06:44 -0700

    Added new code

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
 console.log("Hello World!");
-let num1 = 10;
-let num2 = 20;
-let difference = num2 - num1;
-console.log(difference);
-prompt("Provide a number");
 
-let num3 = 5
-let num4 = 1
-let product = num3 * num4
-console.log(product)
-prompt("Provide another number");
-alert("The difference of the two numbers are: -5");
-prompt("Provide a number");
-console.log(num2);
-prompt("Provide another number");
-console.log(num3);
-alert("The product of the two numbers are: 100")
+let operator1 = parseInt(prompt("Provide a number"));
+let operator2 =  parseInt(prompt("Provide another number"));
+let sum = operator1 + operator2;
+let result;
+if(sum < 10){
+	console.warn("The sum of two numbers is " +sum);
+}
+else if(sum >= 10 && sum <=20){
+	result = operator2 - operator1;
+	alert("The difference of two number is " +result);
+}
+else if(sum >= 21 && sum <= 29){
+	result = operator2 * operator1;
+	alert("The product of two number is " +result);
+}
+else if(sum >= 30){
+	result = operator2 / operator1;
+	alert("The dividend of two numbers is " +result);
+}
+
+let name =prompt("What is your name");
+let age = prompt("What is your age?");
+
+if(name == "" || age ==""){
+	alert("Are you a time traveler");
+
+}
+else {
+	alert("Hello "+ name+ ". " + "Your age is " + age);
+}
+
+function isLegalAge(age){
+
+	if(parseInt(age) >= 18){
+		alert("You are of legal age");
+
+	}
+	else{
+		alert("You are not allowed here");
+	}
+
+}
+
+isLegalAge(age);
+
+switch(parseInt(age)){
+	case 18:
+		alert("You are now allowed to party");
+		break;
+	case 21:
+		alert("You are now part of adult society");
+		break;
+	case 65:
+		alert("We thank you for your contribution to society");
+		break;
+	default:
+		alert("Are you sure you're not an alien");
+		break;
+}
+
+try{
+	alert(isLegalag(18));
+}
+catch (error){
+	console.warn(error.message);
+}
+
