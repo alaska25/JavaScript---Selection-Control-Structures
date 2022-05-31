b7af3682c273e0d0406d5955170d373716f0ee97
Arman <villegasarman23@gmail.com>
2022-05-31 06:31:31 -0700

    Added new code

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+let num1 = 10;
+let num2 = 20;
+let difference = num2 - num1;
+console.log(difference);
+prompt("Provide a number");
+
+let num3 = 5
+let num4 = 1
+let product = num3 * num4
+console.log(product)
+prompt("Provide another number");
+alert("The difference of the two numbers are: -5");
+prompt("Provide a number");
+console.log(num2);
+prompt("Provide another number");
+console.log(num3);
+alert("The product of the two numbers are: 100")
